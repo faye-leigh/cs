c2140f7cbb674e3af4384c14f114d2ef618ca396
Faye Leigh <faye@fayeleigh.com>
2025-02-09 22:00:43 -0500

    finish lab2

---
+++ CSE2221/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("\nWhat is your name?");
+            string name = Console.ReadLine() ?? "Guest";
+            Console.WriteLine($"Hello, {name}.");
+        }
+    }
+}
